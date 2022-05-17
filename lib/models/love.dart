6042e3ddc1b34e2959f9340c77f36df4a6ed1871
Johanu GANDONOU <johanugandonou@gmail.com>
2022-05-17 22:45:40 +0100

    Adding of models

--- lib/models/love.dart
+++ lib/models/love.dart
@@ -1,9 +1,8 @@
-class Comment{
+class Love{
   int? id;
-  String? content;
   int? user_id;
   int? article_id;
   DateTime? date;//date de creation, c'est à dire DateTime.now
 
-  Comment({this.id, this.content, this.user_id, this.article_id, this.date});
+  Love({this.id, this.user_id, this.article_id, this.date});
 }
